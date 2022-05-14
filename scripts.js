d41a7768c1691b1e4f9b9c66f780a93da503b1eb
Stephen Tatum <stephenanthonytatum@gmail.com>
2022-05-14 17:18:30 -0500

    Add player logic

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function computerPlay() {
     let randomInt = getRandomIntInclusive(1,3);
     switch (randomInt) {
-        case 1: return "rock";
-        case 2: return "paper";
-        case 3: return "scissors";
+        case 1: return "Rock";
+        case 2: return "Paper";
+        case 3: return "Scissors";
         default:
     }
 }
@@ -15,6 +15,10 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 // Retuns a lower case selection of rock, paper, or scissors. If canceled null is returned.
 function getPlayerSelection() {
     let input;
@@ -24,13 +28,40 @@ function getPlayerSelection() {
         if (input === null) {
             break;
         }
-        input = input.toLowerCase();
+        input = capitalizeFirstLetter(input.toLowerCase());
     } 
-    while (input !== "rock" && input !== "paper" && input !== "scissors");
+    while (input !== "Rock" && input !== "Paper" && input !== "Scissors");
     
     return input;
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } 
+        if (computerSelection === "Scissors"){
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
+        }
+    } 
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Scissors") {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } 
+        if (computerSelection === "Paper"){
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    }
+    if (playerSelection === "Scissors") {
+        if (computerSelection === "Paper") {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        } 
+        if (computerSelection === "Rock"){
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    }
+    // This must be a tie
+    return `It's a tie! ${playerSelection} ties with ${computerSelection}`;
 }
+
+playRound(getPlayerSelection, computerPlay);
