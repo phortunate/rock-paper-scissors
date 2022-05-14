74d68866b91fc942c9ce387492f0ef511ba3681b
Stephen Tatum <stephenanthonytatum@gmail.com>
2022-05-13 19:03:13 -0500

    Add user input

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function computerPlay() {
     let randomInt = getRandomIntInclusive(1,3);
     switch (randomInt) {
-        case 1: return "Rock";
-        case 2: return "Paper";
-        case 3: return "Scissors";
+        case 1: return "rock";
+        case 2: return "paper";
+        case 3: return "scissors";
         default:
     }
 }
@@ -13,4 +13,24 @@ function getRandomIntInclusive(min, max) {
     min = Math.ceil(min); 
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Retuns a lower case selection of rock, paper, or scissors. If canceled null is returned.
+function getPlayerSelection() {
+    let input;
+    do {
+        input = prompt("Play Rock Paper Scissors! \nWhat's your choice?");
+        // If the prompt is canceled
+        if (input === null) {
+            break;
+        }
+        input = input.toLowerCase();
+    } 
+    while (input !== "rock" && input !== "paper" && input !== "scissors");
+    
+    return input;
+}
+
+function playRound(playerSelection, computerSelection) {
+    
 }
